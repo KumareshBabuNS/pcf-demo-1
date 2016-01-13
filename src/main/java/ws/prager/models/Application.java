e9064690e2d08f2b02244f176d1a3989c2d7c2c3
Bernd Prager <bernd@prager.ws>
2016-01-13 08:58:38 -0500

    remove propertyconfig due jackson error

--- src/main/java/ws/prager/models/Application.java
+++ src/main/java/ws/prager/models/Application.java
@@ -12,13 +12,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
-@Configuration
-@PropertySource(value={"classpath:credentials.properties"})
 @Component
 public class Application {
 
