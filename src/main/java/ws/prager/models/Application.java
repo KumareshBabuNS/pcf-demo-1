6fb70a61558200bb1254953684cc4a997dd7b2af
Bernd Prager <bernd@prager.ws>
2016-01-13 08:31:25 -0500

    separate credential file

--- src/main/java/ws/prager/models/Application.java
+++ src/main/java/ws/prager/models/Application.java
@@ -10,12 +10,20 @@ import org.cloudfoundry.client.lib.CloudFoundryClient;
 import org.cloudfoundry.client.lib.domain.CloudApplication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
+@Configuration
+@PropertySource(value={"classpath:credentials.properties"})
 @Component
 public class Application {
 
+	@Autowired
+	Environment env;
 	@Value("${cfuser.name}")
 	String username;
 	@Value("${cfuser.pwd}")
